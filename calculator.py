dc33f546f342fe204596b2317bccbfab1192fe6d
Sandun-Induranga <sandutharu40@gmail.com>
2022-05-15 22:49:55 +0530

    Event handled Buttons

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from logging import root
+from mimetypes import init
 from tkinter import *
 
 
@@ -14,17 +15,46 @@ class Calculator:
         Entry(width=17, bg='#fff', font=('Arial Bold', 28),
               textvariable=self.equation).place(x=0, y=0)
 
-        xPlace = 0
-        yPlace = 50
-        symbols = ['(', ')', '%', '1', '2', '3', '4', '5', '6', '7',
-                   '8', '9', '0', '.', '+', '-', '/', 'x', '=', 'C']
-        for i in range(1, 21):
-            Button(width=11, height=4, text=symbols[i-1], relief='flat', bg='white',
-                   command=lambda: self.show('')).place(x=xPlace, y=yPlace)
-            xPlace += 90
-            if(i % 4 == 0):
-                yPlace += 75
-                xPlace = 0
+        Button(width=11, height=4, text='(', relief='flat', bg='white',
+               command=lambda: self.show('(')).place(x=0, y=50)
+        Button(width=11, height=4, text=')', relief='flat', bg='white',
+               command=lambda: self.show(')')).place(x=90, y=50)
+        Button(width=11, height=4, text='1', relief='flat', bg='white',
+               command=lambda: self.show(1)).place(x=180, y=50)
+        Button(width=11, height=4, text='2', relief='flat', bg='white',
+               command=lambda: self.show(2)).place(x=0, y=125)
+        Button(width=11, height=4, text='3', relief='flat', bg='white',
+               command=lambda: self.show(3)).place(x=90, y=125)
+        Button(width=11, height=4, text='4', relief='flat', bg='white',
+               command=lambda: self.show(4)).place(x=180, y=125)
+        Button(width=11, height=4, text='5', relief='flat', bg='white',
+               command=lambda: self.show(5)).place(x=0, y=200)
+        Button(width=11, height=4, text='6', relief='flat', bg='white',
+               command=lambda: self.show(6)).place(x=90, y=200)
+        Button(width=11, height=4, text='7', relief='flat', bg='white',
+               command=lambda: self.show(7)).place(x=180, y=200)
+        Button(width=11, height=4, text='8', relief='flat', bg='white',
+               command=lambda: self.show(8)).place(x=0, y=275)
+        Button(width=11, height=4, text='9', relief='flat', bg='white',
+               command=lambda: self.show(9)).place(x=90, y=275)
+        Button(width=11, height=4, text='C', relief='flat', bg='white',
+               command=self.clear).place(x=0, y=350)
+        Button(width=11, height=4, text='0', relief='flat', bg='white',
+               command=lambda: self.show(0)).place(x=180, y=275)
+        Button(width=11, height=4, text='.', relief='flat', bg='white',
+               command=lambda: self.show('.')).place(x=90, y=350)
+        Button(width=11, height=4, text='/', relief='flat', bg='white',
+               command=lambda: self.show('/')).place(x=270, y=50)
+        Button(width=11, height=4, text='x', relief='flat', bg='white',
+               command=lambda: self.show('*')).place(x=270, y=125)
+        Button(width=11, height=4, text='-', relief='flat', bg='white',
+               command=lambda: self.show('-')).place(x=270, y=200)
+        Button(width=11, height=4, text='+', relief='flat', bg='white',
+               command=lambda: self.show('+')).place(x=270, y=275)
+        Button(width=11, height=4, text='%', relief='flat', bg='white',
+               command=lambda: self.show('%')).place(x=180, y=350)
+        Button(width=11, height=4, text='=', relief='flat', bg='white',
+               command=self.solve).place(x=270, y=350)
 
     def show(self, value):
         self.entry_value += str(value)
