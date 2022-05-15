b325d69dbbb881c4ae114037c9b75907da6c5163
Sandun-Induranga <sandutharu40@gmail.com>
2022-05-16 00:23:47 +0530

    Add Styles

--- calculator.py
+++ calculator.py
@@ -15,45 +15,45 @@ class Calculator:
         Entry(width=17, bg='#fff', font=('Arial Bold', 28),
               textvariable=self.equation).place(x=0, y=0)
 
-        Button(width=11, height=4, text='(', relief='flat', bg='white',
+        Button(width=11, height=4, text='(', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('(')).place(x=0, y=50)
-        Button(width=11, height=4, text=')', relief='flat', bg='white',
+        Button(width=11, height=4, text=')', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(')')).place(x=90, y=50)
-        Button(width=11, height=4, text='1', relief='flat', bg='white',
+        Button(width=11, height=4, text='1', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(1)).place(x=180, y=50)
-        Button(width=11, height=4, text='2', relief='flat', bg='white',
+        Button(width=11, height=4, text='2', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(2)).place(x=0, y=125)
-        Button(width=11, height=4, text='3', relief='flat', bg='white',
+        Button(width=11, height=4, text='3', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(3)).place(x=90, y=125)
-        Button(width=11, height=4, text='4', relief='flat', bg='white',
+        Button(width=11, height=4, text='4', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(4)).place(x=180, y=125)
-        Button(width=11, height=4, text='5', relief='flat', bg='white',
+        Button(width=11, height=4, text='5', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(5)).place(x=0, y=200)
-        Button(width=11, height=4, text='6', relief='flat', bg='white',
+        Button(width=11, height=4, text='6', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(6)).place(x=90, y=200)
-        Button(width=11, height=4, text='7', relief='flat', bg='white',
+        Button(width=11, height=4, text='7', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(7)).place(x=180, y=200)
-        Button(width=11, height=4, text='8', relief='flat', bg='white',
+        Button(width=11, height=4, text='8', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(8)).place(x=0, y=275)
-        Button(width=11, height=4, text='9', relief='flat', bg='white',
+        Button(width=11, height=4, text='9', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(9)).place(x=90, y=275)
-        Button(width=11, height=4, text='C', relief='flat', bg='white',
+        Button(width=11, height=4, text='C', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=self.clear).place(x=0, y=350)
-        Button(width=11, height=4, text='0', relief='flat', bg='white',
+        Button(width=11, height=4, text='0', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show(0)).place(x=180, y=275)
-        Button(width=11, height=4, text='.', relief='flat', bg='white',
+        Button(width=11, height=4, text='.', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('.')).place(x=90, y=350)
-        Button(width=11, height=4, text='/', relief='flat', bg='white',
+        Button(width=11, height=4, text='/', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('/')).place(x=270, y=50)
-        Button(width=11, height=4, text='x', relief='flat', bg='white',
+        Button(width=11, height=4, text='x', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('*')).place(x=270, y=125)
-        Button(width=11, height=4, text='-', relief='flat', bg='white',
+        Button(width=11, height=4, text='-', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('-')).place(x=270, y=200)
-        Button(width=11, height=4, text='+', relief='flat', bg='white',
+        Button(width=11, height=4, text='+', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('+')).place(x=270, y=275)
-        Button(width=11, height=4, text='%', relief='flat', bg='white',
+        Button(width=11, height=4, text='%', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=lambda: self.show('%')).place(x=180, y=350)
-        Button(width=11, height=4, text='=', relief='flat', bg='white',
+        Button(width=11, height=4, text='=', relief='flat', bg='#14213d', highlightbackground="black", highlightcolor="black", font="Helvetica 14 bold", fg='#fca311',
                command=self.solve).place(x=270, y=350)
 
     def show(self, value):
